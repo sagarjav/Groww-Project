a6fe8ccef1d79f3ed0ad4487601cc8bc1b8b369f
sagar <technicaltipsinhindi143@gmail.com>
2024-11-18 20:56:28 +0530

    first commit

---
+++ src/test/java/testCases/TC_002_Verify_Logo_is_Displayed.java
@@ -0,0 +1,22 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
+
+public class TC_002_Verify_Logo_is_Displayed extends BaseClass
+{
+	@Test(groups="sanity")
+	public void Verify_Logo_is_Displayed() 
+	{
+			LoginPage Lg=new LoginPage(driver);
+			
+			
+			Assert.assertTrue(Lg.isLogoDipalyed(), "Logo is Displaying: "+ Lg.isLogoDipalyed());
+	
+	
+	}
+	
+
+}
